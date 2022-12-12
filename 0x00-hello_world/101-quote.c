db2cfebf0ee250591dfe23eb7d52f7346004c6e3
Joseyemmy <joeyemmy57@gmail.com>
2022-12-12 12:45:43 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Print out the blast of a quote bin the standard error.
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - dora korpar, 2015-10-19\n", 58);
+	return (1);
+}
